28cb500bb53b5cd66aaf69dd93bf933c3c704b93
朱奇奇(10019477) <zhuqq@glodon.com>
2018-06-27 19:55:03 +0800

    'cleanAll'

--- model-browsing/src/store/mutation-types.js
+++
@@ -1,5 +0,0 @@
-export const SHOW_LOADING = 'SHOW_LOADING'
-export const HIDE_LOADING = 'HIDE_LOADING'
-
-
-export const INIT_PROJECT_ID = 'INIT_PROJECT_ID'
--- src/store/mutation-types.js
+++
@@ -1,1 +0,0 @@
-export const GET_PROJECT_ID = 'GET_PROJECT_ID'
